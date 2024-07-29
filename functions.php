31c6e3f541b874e5e9b472c6f6a46355c189062a
Raphael Borges <raphaelb.designer@gmail.com>
2024-07-29 10:45:49 -0300

    Add Enqueue

--- functions.php
+++ functions.php
@@ -6,6 +6,10 @@ if (!function_exists('add_action')) {
 
 // Variables
 define('START_THEME_DIR', get_template_directory());
+define('START_THEME_URL', get_template_directory_uri());
+define('START_THEME_VERSION', wp_get_theme()->get('Version'));
+
+
 
 // Includes
 
@@ -18,10 +22,12 @@ foreach ($allFiles as $filename) {
 }
 
 // Hooks
+
+add_action('wp_head', 'start_js_check_scripts'); // Check if js is enable - includes/enqueue.php
+add_action('wp_enqueue_scripts', 'start_enqueue_styles');  // Add Styles Theme - includes/enqueue.php
+// add_action('wp_enqueue_scripts', 'start_enqueue_api_scripts'); // - includes/enqueue.php
 add_action('init', 'start_block_init'); //  - blocks/register-blocks.php
 add_filter('block_categories_all', 'start_block_categories', 1, 2); // - blocks/register-blocks.php
 add_action('rest_api_init', 'start_rest_api_init'); // Api Login - includes/api
-add_action('wp_enqueue_scripts', 'start_enqueue_scripts'); // - includes/enqueue.php
-add_action('wp_head', 'start_js_check_scripts'); // Check if js is enable - includes/enqueue.php
 add_action('init', 'start_disable_emojis'); // - includes/utlities/disable_emojis.php
 add_action('wp_enqueue_scripts', 'start_wpcf7_dequeue_scripts', 99); // Optimize contact-form-7 Enqueue  - includes/utlities/contact-form-7.php
--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,6 +1,6 @@
 <?php
 
-function start_enqueue_scripts()
+function start_enqueue_api_scripts()
 {
   $authURLs = json_encode([
     'signup' => esc_url_raw(rest_url('start/v1/signup')),
@@ -17,8 +17,19 @@ function start_enqueue_scripts()
 function start_js_check_scripts()
 {
 ?>
-  <script>
-    document.documentElement.className += ' js';
-  </script>
+<script>
+document.documentElement.className += ' js';
+</script>
 <?php
 }
+
+function start_enqueue_styles()
+{
+  wp_enqueue_style('start_theme_style', START_THEME_URL . '/assets/css/styles.min.css', array(), START_THEME_VERSION);
+}
+
+function start_enqueue_scripts()
+{
+  wp_enqueue_script('start_theme_script', START_THEME_URL . '/assets/js/index.min.js', array(), START_THEME_VERSION, true);
+}
+add_action('wp_enqueue_scripts', 'start_enqueue_scripts');
